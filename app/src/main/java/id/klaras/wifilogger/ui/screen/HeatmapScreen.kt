85f9d52f05e2245e1cccd87c1afd6fabd6a20530
Waris Agung Widodo <ido.alit@gmail.com>
2026-02-12 14:42:54 +0700

    Add padding to HeatmapScreen to accommodate status bars

--- app/src/main/java/id/klaras/wifilogger/ui/screen/HeatmapScreen.kt
+++ app/src/main/java/id/klaras/wifilogger/ui/screen/HeatmapScreen.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -78,6 +81,7 @@ fun HeatmapScreen(
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .padding(WindowInsets.statusBars.asPaddingValues())
             .padding(16.dp)
             .verticalScroll(rememberScrollState())
     ) {
